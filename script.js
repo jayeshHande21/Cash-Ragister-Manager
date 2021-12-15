e7c3332476af01b179c595e07f3ebf92f2566110
Jayu <jayeshhande00@gmail.com>
2021-12-15 14:40:17 +0530

    initial commit(index file)

---
+++ script.js
@@ -0,0 +1,15 @@
+const billAmount = document.querySelector("#bill-amount");
+
+const cashGiven = document.querySelector("#cash-given");
+
+const button = document.querySelector("#button");
+
+const msg = document.querySelector("#error");
+
+button.addEventListener("click", validateAmount());
+
+function validateAmount(){
+
+    console.log("clicked");
+
+}
